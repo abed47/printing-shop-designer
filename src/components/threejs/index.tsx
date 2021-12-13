7b04fb3cc5d621a8c8617900d7a0b47bfd05c81a
Abdull-Rahman <abed472011@gmail.com>
2021-12-13 16:29:55 +0200

    updates

--- src/components/threejs/index.tsx
+++ src/components/threejs/index.tsx
@@ -1,7 +1,25 @@
-const ThreeComponent = (props: any) => {
+import { Canvas } from '@react-three/fiber'
+// import { OrbitControls } from 'drei'
+import {  Plane, OrbitControls } from '@react-three/drei';
+
+const Box = () => {
+    return <mesh>
+        <boxBufferGeometry attach="geometry" />
+        <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+}
+  
+const ThreeComponent = (props:any) => {
     return (
         <div className="three-wrapper">
-            hello from 3
+            <Canvas>
+      {/* <ambientLight /> */}
+      {/* <pointLight position={[10, 10, 10]} /> */}
+      {/* <Plane args={[2, 2]} /> */}
+      <Box/>
+      <Box />
+      <OrbitControls />
+    </Canvas>
         </div>
     );
 }
